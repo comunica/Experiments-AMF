175f83c74b6a2920a1067f61c62fd8ce3eff3e7e
Ruben Taelman <ruben.taelman@ugent.be>
2019-03-11 13:28:15 +0100

    Fix plot of C queries

--- plot.sh
+++ plot.sh
@@ -4,7 +4,7 @@
 
 for experiment in $(ls results); do
     pushd results/$experiment
-    comunica-bencher plot queries -q '^[^C]' -n plot_c combination_* && tex2svg plot_c.tex
+    comunica-bencher plot queries -q '^C' -n plot_c combination_* && tex2svg plot_c.tex
     comunica-bencher plot queries -q '^[^C]' -n plot_no_c combination_* && tex2svg plot_no_c.tex
     popd
 done
